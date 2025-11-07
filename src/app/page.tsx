2fd8d62039b04cf7c5cc2741050b029a5cfc65ae
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-11-07 16:25:47 +0100

    build(ci): testing node.js workflow configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,30 @@
 export default async function Home() {
-  const name = "Rachel";
-  const user = await fetchUser(name);
+  // const name = "Rachel";
+  // const user = await fetchUser(name);
 
   return (
     <>
-      <div>{`${user.username} lives at ${user.address}, ${user.postal_code} ${user.country}.`}</div>
+      Voici la home.
+      {/* <div>{`${user.username} lives at ${user.address}, ${user.postal_code} ${user.country}.`}</div> */}
     </>
   );
 }
 
-async function fetchUser(username: string) {
-  const baseUrl = "http://127.0.0.1:8000";
+// async function fetchUser(username: string) {
+//   const baseUrl = "http://127.0.0.1:8000";
 
-  try {
-    const response = await fetch(`${baseUrl}/users/get-user/${username}`);
-    if (!response.ok) {
-      console.error("⛔️ Response:", response); // TO DO: get error code
-      throw new Error("Failed to fetch data");
-    }
-    const user = await response.json();
-    return user;
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    return null;
-  }
-}
+//   try {
+//     const response = await fetch(`${baseUrl}/users/get-user/${username}`);
+//     if (!response.ok) {
+//       console.error("⛔️ Response:", response); // TO DO: get error code
+//       throw new Error("Failed to fetch data");
+//     }
+//     const user = await response.json();
+//     return user;
+//   } catch (error) {
+//     console.error("Error fetching user:", error);
+//     return null;
+//   }
+// }
 
 // TO DO: if 404, user not found
