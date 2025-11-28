4b80c3b751c7344fa2ff5a22f86b296da8f44354
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-11-28 10:53:31 +0100

    refactor(main): rearrange file tree

---
+++ src/app/(main)/profile/Profile.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const Profile = () => {
+  const [bookForm, setBookForm] = useState({
+    title: '',
+    author: '',
+    description: '',
+    published_year: 1997,
+    image_url: '',
+  });
+
+  return <></>;
+};
+
+export default Profile;
