d235bfd12ce38541043176a3281814e394750075
annekaftal <anne.kaftal@gmail.com>
2025-12-05 16:43:54 +0100

    feat: add border radius to book images

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -7,23 +7,23 @@ import {
   ImageListItemBar,
   Stack,
   Typography,
-} from '@mui/material';
-import { Book, Category } from '../../types';
-import { logger } from '../../../lib/logger';
+} from "@mui/material";
+import { Book, Category } from "../../types";
+import { logger } from "../../../lib/logger";
 
 const Home = async () => {
-  const baseUrl = 'http://127.0.0.1:8000';
+  const baseUrl = "http://127.0.0.1:8000";
 
   const fetchBooks = async () => {
     try {
       const response = await fetch(`${baseUrl}/books/get-books`);
       if (!response.ok) {
-        throw new Error('Failed to fetch books data');
+        throw new Error("Failed to fetch books data");
       }
       const books = await response.json();
       return books;
     } catch (error) {
-      console.error('Error fetching all books:', error);
+      console.error("Error fetching all books:", error);
       return null;
     }
   };
@@ -32,12 +32,12 @@ const Home = async () => {
     try {
       const response = await fetch(`${baseUrl}/books/get-categories`);
       if (!response.ok) {
-        throw new Error('Failed to fetch categories data');
+        throw new Error("Failed to fetch categories data");
       }
       const categories = await response.json();
       return categories;
     } catch (error) {
-      console.error('Error fetching all categories:', error);
+      console.error("Error fetching all categories:", error);
       return null;
     }
   };
@@ -45,45 +45,46 @@ const Home = async () => {
   const books = await fetchBooks();
   const categories = await fetchCategories();
 
-  // Test log levels
-  logger.trace('Trace test');
-  logger.debug('Debug test');
-  logger.info('Info test');
-  logger.warn('Warn test');
-  logger.error('Error test');
-  logger.fatal('Fatal test');
-
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth="lg">
       <Box
         sx={{
-          width: '100%',
-          height: '2em',
-          marginBottom: '3em',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          overflow: 'scroll',
-          justifyItems: 'center',
+          width: "100%",
+          height: "2em",
+          marginBottom: "3em",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "scroll",
+          justifyItems: "center",
+          scrollbarWidth: "none",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
         }}
       >
         <Stack
-          direction='row'
+          direction="row"
           spacing={2}
           sx={{
-            flexWrap: 'nowrap',
-            width: 'max-content',
-            paddingBottom: 1,
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            width: "100%",
           }}
         >
           {categories &&
             categories.map((category: Category, index: number) => (
               <Button
                 key={index}
-                size='small'
-                variant='contained'
+                size="small"
+                variant="outlined"
                 sx={{
-                  whiteSpace: 'nowrap',
-                  minWidth: '150px',
+                  whiteSpace: "nowrap",
+                  minWidth: "150px",
+                  textTransform: "none",
+                  fontFamily: "Poppins",
+                  fontWeight: 600,
+                  borderRadius: "10px",
                   flexShrink: 0,
                 }}
               >
@@ -93,24 +94,45 @@ const Home = async () => {
         </Stack>
       </Box>
 
-      <Typography sx={{ fontWeight: 'bold', my: 2 }}>
+      <Typography sx={{ fontWeight: "bold", my: 2 }}>
         Récemment ajoutés
       </Typography>
 
-      <ImageList sx={{ width: '100%', height: '100%' }} cols={6}>
+      <ImageList
+        sx={{
+          width: "100%",
+          height: "100%",
+        }}
+        cols={6}
+        gap={8}
+      >
         {books &&
           books.map((book: Book, index: number) => (
-            <ImageListItem key={index} sx={{ height: 140, width: 150 }}>
+            <ImageListItem
+              key={index}
+              sx={{
+                aspectRatio: "4 / 5",
+                maxWidth: "185px",
+                mx: "auto",
+              }}
+            >
               <img
                 srcSet={book.image_url}
                 src={book.image_url}
                 alt={book.title}
-                loading='lazy'
+                loading="lazy"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderRadius: "10px",
+                  border: "1px solid black",
+                }}
               />
               <ImageListItemBar
                 title={book.title}
                 subtitle={book.author}
-                position='below'
+                position="below"
               />
             </ImageListItem>
           ))}
