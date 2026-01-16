9b69bb404986eb3f35d23e76b30d1d5519b27da2
annekaftal <anne.kaftal@gmail.com>
2026-01-16 10:33:01 +0100

    feat(home): adjust responsive grid for book display

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -96,7 +96,7 @@ const Home = async () => {
           </Typography>
           <Grid
             container
-            columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}
+            columns={{ xs: 2, sm: 4, md: 5, lg: 5, xl: 6 }}
             rowSpacing={3}
             columnSpacing={2.5}
           >
