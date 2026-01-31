bf20ed55b500d5e413812b38196adb5155000925
annekaftal <anne.kaftal@gmail.com>
2026-01-31 17:57:09 +0100

    feat(books): mobile version of book detail page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { Header } from "../components/Header";
+import { Stack } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,13 +15,7 @@ export default function HomeLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-      }}
-    >
+    <main>
       <Header />
       {children}
     </main>
