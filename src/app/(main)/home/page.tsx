8f2101570cd3b55a42a1d6e74f570338f2ef28fa
anaisrandria <anais.randriamitantsoa@gmail.com>
2026-02-05 21:30:40 +0100

    fix(style): add Poppins font to global theme

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -60,7 +60,6 @@ const Home = () => {
                     whiteSpace: 'nowrap',
                     minWidth: '150px',
                     textTransform: 'none',
-                    fontFamily: 'Poppins',
                     fontWeight: 600,
                     borderRadius: '10px',
                     flexShrink: 0,
@@ -77,7 +76,6 @@ const Home = () => {
             sx={{
               fontsize: '14px',
               fontWeight: '600',
-              fontFamily: 'Poppins',
             }}
           >
             {'Récemment ajoutés'}
